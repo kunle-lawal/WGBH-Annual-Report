8e0b34094f5d60bf7e91c386a7201078e55e91ab
Kunle Lawal <kunle_lawal@wgbh.org>
2019-02-27 11:22:56 -0500

    More TIcket work.
    Added navigation color.
    Added image scrolling for hero.

--- scripts/main/main.js
+++ scripts/main/main.js
@@ -7,7 +7,7 @@
             var articlePos = [getOffSetVal(".truth-transparency"), getOffSetVal(".science"), getOffSetVal(".educating-youth"), getOffSetVal(".town-square")];
 
             // console.log(articlePos);
-            // console.log(curtain_height);
+            console.log(curtain_height);
             if (document.scrollingElement.scrollTop >= curtain_height){
                 $(".hero-window").css({
                     "position": "relative",
@@ -30,32 +30,37 @@
             }
 
             console.log(scroll_element.scrollTop(), articlePos[0]);
-            if (scroll_element.scrollTop() > getOffSetVal("#financial")) {
+            if (document.scrollingElement.scrollTop > getOffSetVal(".logo_farm")) {
+                $("nav").css({
+                    "background-color": "#222222",
+                    "color": "white"
+                });
+            } else if (document.scrollingElement.scrollTop > getOffSetVal("#financial")) {
                 $("nav").css({
                     "background-color": "#03507c",
                     "color": "white"
                 });
-            } else if (scroll_element.scrollTop() > articlePos[3]) {
+            } else if (document.scrollingElement.scrollTop > articlePos[3]) {
                 $("nav").css({
                     "background-color": "#20b5bf",
-                    "color": "#222222"
+                    "color": "#white"
                 });
-            } else if (scroll_element.scrollTop() > articlePos[2]) {
+            } else if (document.scrollingElement.scrollTop > articlePos[2]) {
                 $("nav").css({
                     "background-color": "#f7922a",
                     "color": "#ffffff"
                 });
-            } else if (scroll_element.scrollTop() > articlePos[1]) {
+            } else if (document.scrollingElement.scrollTop > articlePos[1]) {
                 $("nav").css({
                     "background-color": "#6e3078",
                     "color": "white"
                 });
-            } else if (scroll_element.scrollTop() > articlePos[0]) {
+            } else if (document.scrollingElement.scrollTop > articlePos[0]) {
                 $("nav").css({
                     "background-color": "#d83300",
                     "color": "white"
                 });
-            } else if (scroll_element.scrollTop() > getOffSetVal(".link")) {
+            } else if (document.scrollingElement.scrollTop > getOffSetVal(".link")) {
                 $("nav").css({
                     "background-color": "#222222",
                     "color": "white"
@@ -89,9 +94,11 @@
 
         $(".mobile_button").click(function(){
             $(".nav-items-container").toggleClass("slideInDown");
+            $(".mobile_button").toggleClass("mobile_button_clicked");
         });
         $(".nav-items").click(function () {
             $(".slideInDown").toggleClass("slideInDown");
+            $(".mobile_button").toggleClass("mobile_button_clicked");
         });
     });
 
